c06fa7b5b708e32258a2ec3aedf53b8609392e43
Eric Busboom <eric.busboom@jointheleague.org>
2025-06-12 01:02:52 +0000

    Update Dockerfile and devcontainer configuration; add syllabus extension installation and replace Getting Started README with Jupyter Notebook

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+TARGET_DIR=$1
+THIS_DIR=$(dirname "$(realpath "$0")")
+
+SYLLABUS_EXTENSION=https://github.com/league-infrastructure/league-vscode-ext/releases/download/v1.20250510.2/jtl-syllabus-v1.20250510.2.vsix
+
+
 pip3 install --upgrade pip
 pip3 install  -r requirements.txt
 
 git config --global pull.rebase true
 
-echo "export PYTHONPATH=$(pwd)/.lib/:$PYTHONPATH" >> ~/.zshrc
+# Put a newline in the prompt to make it easier to read.
 echo "export PYTHONPATH=$(pwd)/.lib/:$PYTHONPATH" >> ~/.bashrc
-echo "export PYTHONPATH=$(pwd)/.lib/:$PYTHONPATH" >> ~/.profile
 
-source ~/.bashrc
 
+# Fetch the syllabus extension to /vscode/extensionsCache/
+mkdir -p /vscode/extensionsCache/
+curl -L $SYLLABUS_EXTENSION -o /vscode/extensionsCache/$(basename $SYLLABUS_EXTENSION)
+vsix_file="/vscode/extensionsCache/$(basename $SYLLABUS_EXTENSION)"
+base_name=$(basename "$SYLLABUS_EXTENSION" .vsix)
+target_dir="$TARGET_DIR/.vscode/extensions/$base_name"
+mkdir -p "$target_dir"
+unzip -q "$vsix_file" -d "$target_dir"
 
